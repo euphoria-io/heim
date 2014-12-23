f239ddb79cfa6b1e1946a67af64eaf06a4e2cd14
Max Goodman <c@chromakode.com>
2014-12-23 02:16:29 -0800

    Experiment: color time column based on hour of day.

--- client/lib/ui/message.js
+++ client/lib/ui/message.js
@@ -14,10 +14,16 @@ module.exports = React.createClass({
   render: function() {
     var message = this.props.message
     var time = moment.unix(message.get('time'))
+    var hour = time.hour()
+    var bgLightness = (hour > 12 ? 24 - hour : hour) / 12
+    var timeStyle = {
+      background: 'hsla(0, 0%, ' + (100 * bgLightness).toFixed(2) + '%, .175)',
+      color: 'hsla(0, 0%, 100%, ' + (.3 + .2 * bgLightness).toFixed(2) + ')',
+    }
 
     return (
       <div className="line">
-        <time dateTime={time.toISOString()} title={time.format('MMMM Do YYYY, h:mm:ss a')}>
+        <time dateTime={time.toISOString()} title={time.format('MMMM Do YYYY, h:mm:ss a')} style={timeStyle}>
           {time.format('h:mma')}
         </time>
         <span className="nick" style={{background: 'hsl(' + message.getIn(['sender', 'hue']) + ', 65%, 85%)'}}>{message.getIn(['sender', 'name'])}</span>
