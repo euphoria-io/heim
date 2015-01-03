e74a69ef8ba92be48a93b87b98be61bc8a311b96
Logan Hanks <logan.hanks@gmail.com>
2015-01-02 16:00:06 -0800

    Revert "Fix empty DB upgrade process and make idempotent."
    This reverts commit 9c869107bddc3eeab078be02b892237116447ed2.

--- backend/persist/backend.go
+++ backend/persist/backend.go
@@ -65,19 +65,13 @@ func (b *Backend) start() {
 }
 
 func (b *Backend) UpgradeDB() error {
-	// TODO: inspect existing schema and adapt
+	// TODO: inspect existing schema and adapt; for now, assume empty DB
+	//return b.createSchema()
 
 	// TEMPORARY HACK: upgrade older messages table
-	oldExists, err := b.DbMap.SelectInt("SELECT 1 FROM pg_class WHERE relname = 'message'")
+	_, err := b.DbMap.Exec("ALTER TABLE message RENAME TO messagearchive")
 	if err != nil {
 		return err
-	}
-
-	if oldExists == 1 {
-		_, err := b.DbMap.Exec("ALTER TABLE message RENAME TO messagearchive")
-		if err != nil {
-			return err
-		}
 	}
 
 	_, err = b.DbMap.Exec("CREATE TABLE message (room text not null, id text not null, parent text, posted timestamp with time zone, sender_id text, sender_name text, content text, primary key (room, id))")
@@ -85,41 +79,30 @@ func (b *Backend) UpgradeDB() error {
 		return err
 	}
 
-	b.DbMap.CreateTablesIfNotExists()
+	type oldmsg struct {
+		Room       string
+		Posted     time.Time
+		SenderID   string `db:"sender_id"`
+		SenderName string `db:"sender_name"`
+		Content    string
+	}
 
-	if oldExists == 1 {
-		type oldmsg struct {
-			Room       string
-			ID         string
-			Parent     string
-			Posted     time.Time
-			SenderID   string `db:"sender_id"`
-			SenderName string `db:"sender_name"`
-			Content    string
+	rows, err := b.DbMap.Select(oldmsg{}, "select * from messagearchive")
+	if err != nil {
+		return err
+	}
+
+	for _, row := range rows {
+		omsg := row.(*oldmsg)
+		msg := &Message{
+			ID:         backend.NewSnowflakeFromTime(omsg.Posted).String(),
+			Room:       omsg.Room,
+			Posted:     omsg.Posted,
+			SenderID:   omsg.SenderID,
+			SenderName: omsg.SenderName,
+			Content:    omsg.Content,
 		}
-
-		rows, err := b.DbMap.Select(oldmsg{}, "SELECT * FROM messagearchive")
-		if err != nil {
-			return err
-		}
-
-		for _, row := range rows {
-			omsg := row.(*oldmsg)
-			msg := &Message{
-				ID:         backend.NewSnowflakeFromTime(omsg.Posted).String(),
-				Room:       omsg.Room,
-				Posted:     omsg.Posted,
-				SenderID:   omsg.SenderID,
-				SenderName: omsg.SenderName,
-				Content:    omsg.Content,
-			}
-			if err := b.DbMap.Insert(msg); err != nil {
-				return err
-			}
-		}
-
-		_, err = b.DbMap.Exec("DROP TABLE messagearchive")
-		if err != nil {
+		if err := b.DbMap.Insert(msg); err != nil {
 			return err
 		}
 	}
