e9e5c041c7d907b124032ba3b61e9ca56ad606fb
Max Goodman <c@chromakode.com>
2015-01-22 21:55:36 -0800

    Dry up message UI render tests.

--- client/test/message-ui-test.js
+++ client/test/message-ui-test.js
@@ -9,7 +9,7 @@ describe('<Message>', function() {
   var Message = require('../lib/ui/message')
   var testTree
 
-  it('automatically links urls', function() {
+  function renderMessage(content) {
     testTree = new Tree('time').reset([
       {
         'id': 'id1',
@@ -18,7 +18,7 @@ describe('<Message>', function() {
           'id': '32.64.96.128:12345',
           'name': 'tester',
         },
-        'content': 'http://google.com',
+        'content': content
       }
     ])
 
@@ -26,52 +26,24 @@ describe('<Message>', function() {
       <Message tree={testTree} nodeId="id1" depth={0} />
     )
 
-    var messageContent = TestUtils.findRenderedDOMComponentWithClass(message, 'message')
+    return TestUtils.findRenderedDOMComponentWithClass(message, 'message')
+  }
+
+  it('automatically links urls', function() {
+    var messageContent = renderMessage('http://google.com')
     assert.equal(messageContent.getDOMNode().innerHTML,
       '<a href="http://google.com" target="_blank" rel="noreferrer">google.com</a>')
   })
 
   it('truncates long urls', function() {
-    var testTree = new Tree('time').reset([
-      {
-        'id': 'id1',
-        'time': 123456,
-        'sender': {
-          'id': '32.64.96.128:12345',
-          'name': 'tester',
-        },
-        'content': 'http://google.com/abcdefghijklmnopqrstuvwxyz1234567890',
-      }
-    ])
-
-    var message = TestUtils.renderIntoDocument(
-      <Message tree={testTree} nodeId="id1" depth={0} />
-    )
-
-    var messageContent = TestUtils.findRenderedDOMComponentWithClass(message, 'message')
+    var messageContent = renderMessage('http://google.com/abcdefghijklmnopqrstuvwxyz1234567890')
     assert.equal(messageContent.getDOMNode().innerHTML,
       '<a href="http://google.com/abcdefghijklmnopqrstuvwxyz1234567890" target="_blank" rel="noreferrer">google.com/abcdefghijklmnopqrstuvwxyz1..</a>')
   })
 
   it('doesn\'t linkify javascript:// links', function() {
     // note: jshint warns about javascript:// URLs
-    var testTree = new Tree('time').reset([
-      {
-        'id': 'id1',
-        'time': 123456,
-        'sender': {
-          'id': '32.64.96.128:12345',
-          'name': 'tester',
-        },
-        'content': 'Javascript://hello javascript://world',  // jshint ignore:line
-      }
-    ])
-
-    var message = TestUtils.renderIntoDocument(
-      <Message tree={testTree} nodeId="id1" depth={0} />
-    )
-
-    var messageContent = TestUtils.findRenderedDOMComponentWithClass(message, 'message')
+    var messageContent = renderMessage('Javascript://hello javascript://world')  // jshint ignore:line
     assert.equal(messageContent.getDOMNode().innerHTML,
       'Javascript://hello javascript://world')  // jshint ignore:line
   })
