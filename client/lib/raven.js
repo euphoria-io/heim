9f819384f43cf972e8868b03aff06cb83e983170
Max Goodman <c@chromakode.com>
2015-03-09 13:16:00 -0700

    Enable native raven-js plugin.

--- client/lib/raven.js
+++ client/lib/raven.js
@@ -1,4 +1,5 @@
 Raven = require('raven-js')
+require('raven-js/plugins/native')
 Raven.config(process.env.SENTRY_ENDPOINT, {
   release: process.env.HEIM_RELEASE,
   tags: {'git_commit': process.env.HEIM_GIT_COMMIT},
