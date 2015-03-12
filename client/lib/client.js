4e6f4cd2f6aad1e2bc9cbd50b616dc100b29ed4b
Max Goodman <c@chromakode.com>
2015-03-12 16:34:53 -0700

    Bootstrap iframe inside main.js script.
    This removes dependence upon inline scripts, allowing us to disallow
    them via CSP.

--- client/lib/client.js
+++ client/lib/client.js
@@ -1,198 +1,207 @@
-// >:)
-window.uiwindow = window.top
-window.uidocument = window.top.document
-
-var _ = require('lodash')
-require('setimmediate')
-
-var EventListeners = require('./eventlisteners')
-
-
-var evs = new EventListeners()
-
-Heim = {
-  addEventListener: evs.addEventListener.bind(evs),
-  removeEventListener: evs.removeEventListener.bind(evs),
-
-  actions: require('./actions'),
-  socket: require('./stores/socket'),
-  chat: require('./stores/chat'),
-  notification: require('./stores/notification'),
-  storage: require('./stores/storage'),
-  focus: require('./stores/focus'),
-  update: require('./stores/update'),
-  plugins: require('./stores/plugins'),
-
-  setFavicon: _.partial(require('./setfavicon'), uidocument),
-
-  // http://stackoverflow.com/a/6447935
-  isTouch: 'ontouchstart' in window,
-  isChrome: /chrome/i.test(navigator.userAgent),
-  isAndroid: /android/i.test(navigator.userAgent),
-  isiOS: /ipad|iphone|ipod/i.test(navigator.userAgent),
+function writeEnv(doc, hash) {
+  var query = hash ? '?v=' + hash : ''
+  doc.write('<script src="/static/raven.js' + query +  '"></script>')
+  doc.write('<script src="/static/main.js' + query +  '"></script>')
+  doc.write('<link rel="stylesheet" type="text/css" id="css" href="/static/main.css' + query + '">')
+  doc.close()
 }
 
-Heim.hook = Heim.plugins.hook
+if (!window.frameElement) {
+  writeEnv(document.getElementById('env').contentWindow.document)
+} else {
+  // >:)
+  window.uiwindow = window.top
+  window.uidocument = window.top.document
 
-if (location.hash == '#perf') {
-  var React = require('react/addons')
-  if (React.addons && React.addons.Perf) {
-    uiwindow.ReactPerf = React.addons.Perf
-    uiwindow.ReactPerf.start()
-  }
-}
+  var _ = require('lodash')
+  require('setimmediate')
 
-var roomName = location.pathname.match(/(\w+)\/$/)[1]
+  var EventListeners = require('./eventlisteners')
 
-Heim.attachUI = function(hash) {
-  var Reflux = require('reflux')
 
-  // IE9+ requires this bind: https://msdn.microsoft.com/en-us/library/ie/gg622930(v=vs.85).aspx
-  Reflux.nextTick(setImmediate.bind(window))
+  var evs = new EventListeners()
 
-  var React = require('react/addons')
-  var SyntheticKeyboardEvent = require('react/lib/SyntheticKeyboardEvent')
-  var Main = require('./ui/main')
+  Heim = {
+    addEventListener: evs.addEventListener.bind(evs),
+    removeEventListener: evs.removeEventListener.bind(evs),
 
-  uidocument.title = roomName
+    actions: require('./actions'),
+    socket: require('./stores/socket'),
+    chat: require('./stores/chat'),
+    notification: require('./stores/notification'),
+    storage: require('./stores/storage'),
+    focus: require('./stores/focus'),
+    update: require('./stores/update'),
+    plugins: require('./stores/plugins'),
 
-  var cssEl = uidocument.getElementById('css')
-  var cssURL = '/static/main.css' + (hash ? '?v=' + hash : '')
-  if (cssEl.parentNode != uidocument.head || cssEl.getAttribute('href') != cssURL) {
-    var newCSSEl = cssEl.cloneNode()
-    newCSSEl.href = cssURL
-    cssEl.id = 'css-old'
-    uidocument.head.appendChild(newCSSEl)
+    setFavicon: _.partial(require('./setfavicon'), uidocument),
 
-    // allow both stylesheets to coexist briefly in an attempt to avoid FOUSC
-    setTimeout(function() {
-      cssEl.parentNode.removeChild(cssEl)
-    }, 30)
-  }
-
-  Heim.addEventListener(uiwindow, 'storage', Heim.storage.storageChange, false)
-
-  Heim.addEventListener(uiwindow, 'focus', Heim.focus.windowFocused, false)
-  Heim.addEventListener(uiwindow, 'blur', Heim.focus.windowBlurred, false)
-  if (uidocument.hasFocus()) {
-    Heim.focus.windowFocused()
+    // http://stackoverflow.com/a/6447935
+    isTouch: 'ontouchstart' in window,
+    isChrome: /chrome/i.test(navigator.userAgent),
+    isAndroid: /android/i.test(navigator.userAgent),
+    isiOS: /ipad|iphone|ipod/i.test(navigator.userAgent),
   }
 
-  Heim.addEventListener(uidocument.body, 'keypress', function(ev) {
-    if (ev.target.nodeName == 'INPUT' &&
-         (ev.target.type == 'text' || ev.target.type == 'password')) {
-      return
+  Heim.hook = Heim.plugins.hook
+
+  if (location.hash == '#perf') {
+    var React = require('react/addons')
+    if (React.addons && React.addons.Perf) {
+      uiwindow.ReactPerf = React.addons.Perf
+      uiwindow.ReactPerf.start()
+    }
+  }
+
+  var roomName = location.pathname.match(/(\w+)\/$/)[1]
+
+  Heim.attachUI = function() {
+    var Reflux = require('reflux')
+
+    // IE9+ requires this bind: https://msdn.microsoft.com/en-us/library/ie/gg622930(v=vs.85).aspx
+    Reflux.nextTick(setImmediate.bind(window))
+
+    var React = require('react/addons')
+    var SyntheticKeyboardEvent = require('react/lib/SyntheticKeyboardEvent')
+    var Main = require('./ui/main')
+
+    uidocument.title = roomName
+
+    var cssEl = uidocument.getElementById('css')
+    var cssURL = document.getElementById('css').getAttribute('href')
+    if (cssEl.parentNode != uidocument.head || cssEl.getAttribute('href') != cssURL) {
+      var newCSSEl = cssEl.cloneNode()
+      newCSSEl.href = cssURL
+      cssEl.id = 'css-old'
+      uidocument.head.appendChild(newCSSEl)
+
+      // allow both stylesheets to coexist briefly in an attempt to avoid FOUSC
+      setTimeout(function() {
+        cssEl.parentNode.removeChild(cssEl)
+      }, 30)
     }
 
-    if (!ev.which) {
-      return
+    Heim.addEventListener(uiwindow, 'storage', Heim.storage.storageChange, false)
+
+    Heim.addEventListener(uiwindow, 'focus', Heim.focus.windowFocused, false)
+    Heim.addEventListener(uiwindow, 'blur', Heim.focus.windowBlurred, false)
+    if (uidocument.hasFocus()) {
+      Heim.focus.windowFocused()
     }
 
-    var character = String.fromCharCode(ev.which)
-    if (character && /\S/.test(character)) {
-      Heim.actions.focusEntry(character)
-    }
-  }, true)
+    Heim.addEventListener(uidocument.body, 'keypress', function(ev) {
+      if (ev.target.nodeName == 'INPUT' &&
+           (ev.target.type == 'text' || ev.target.type == 'password')) {
+        return
+      }
 
-  Heim.addEventListener(uidocument.body, 'keydown', function(ev) {
-    if (ev.target.nodeName == 'INPUT') {
-      return
-    }
+      if (!ev.which) {
+        return
+      }
 
-    // prevent backspace from navigating the page
-    if (ev.which == 8) {
-      ev.preventDefault()
-    }
+      var character = String.fromCharCode(ev.which)
+      if (character && /\S/.test(character)) {
+        Heim.actions.focusEntry(character)
+      }
+    }, true)
 
-    // dig into React a little so it normalizes the event (namely ev.key).
-    var reactEvent = new SyntheticKeyboardEvent(null, null, ev)
-    Heim.actions.keydownOnEntry(reactEvent)
-  }, false)
+    Heim.addEventListener(uidocument.body, 'keydown', function(ev) {
+      if (ev.target.nodeName == 'INPUT') {
+        return
+      }
 
-  if (Heim.isTouch) {
-    React.initializeTouchEvents()
-    uidocument.body.classList.add('touch')
+      // prevent backspace from navigating the page
+      if (ev.which == 8) {
+        ev.preventDefault()
+      }
 
-    Heim.addEventListener(uidocument.body, 'touchstart', function(ev) {
-      ev.target.classList.add('touching')
+      // dig into React a little so it normalizes the event (namely ev.key).
+      var reactEvent = new SyntheticKeyboardEvent(null, null, ev)
+      Heim.actions.keydownOnEntry(reactEvent)
     }, false)
 
-    Heim.addEventListener(uidocument.body, 'touchend', function(ev) {
-      ev.target.classList.remove('touching')
-    }, false)
+    if (Heim.isTouch) {
+      React.initializeTouchEvents()
+      uidocument.body.classList.add('touch')
+
+      Heim.addEventListener(uidocument.body, 'touchstart', function(ev) {
+        ev.target.classList.add('touching')
+      }, false)
+
+      Heim.addEventListener(uidocument.body, 'touchend', function(ev) {
+        ev.target.classList.remove('touching')
+      }, false)
+    }
+
+    setImmediate(function() {
+      Heim.ui = React.render(
+        <Main />,
+        uidocument.getElementById('container')
+      )
+      uidocument.body.classList.add('ready')
+    })
+    window.top.Heim = Heim
+    window.top.require = require
+  }
+
+  Heim.detachUI = function() {
+    uidocument.body.classList.remove('ready')
+    evs.removeAllEventListeners()
+    Heim.ui.unmountComponent()
+  }
+
+  Heim.prepareUpdate = function(hash) {
+    Heim.update.setReady(false)
+
+    var frame = uidocument.getElementById('env-update')
+    if (frame) {
+      frame.parentNode.removeChild(frame)
+    }
+
+    frame = uidocument.createElement('iframe')
+    frame.id = 'env-update'
+    frame.className = 'js'
+    uidocument.body.appendChild(frame)
+
+    frame.contentDocument.open()
+    var context = frame.contentWindow
+    context.onReady = function() {
+      var removeListener = context.Heim.chat.store.listen(function(chatState) {
+        if (chatState.joined) {
+          removeListener()
+
+          // let go of #container
+          Heim.detachUI()
+
+          // attach new React component to #container
+          context.Heim.attachUI(hash)
+          frame.id = 'env'
+
+          // goodbye, world!
+          window.frameElement.parentNode.removeChild(window.frameElement)
+        } else if (chatState.canJoin) {
+          Heim.update.setReady(true, context.Heim.actions.joinRoom)
+        } else {
+          Heim.update.setReady(false)
+        }
+      })
+      context.Heim.actions.connect(roomName)
+    }
+    writeEnv(context.document, hash)
+  }
+
+  Heim.plugins.load(roomName)
+
+  if (!window.onReady) {
+    Heim.actions.connect(roomName)
+    Heim.actions.joinRoom()
   }
 
   setImmediate(function() {
-    Heim.ui = React.render(
-      <Main />,
-      uidocument.getElementById('container')
-    )
-    uidocument.body.classList.add('ready')
+    if (window.onReady) {
+      window.onReady()
+    } else {
+      Heim.attachUI()
+    }
   })
-  window.top.Heim = Heim
-  window.top.require = require
 }
-
-Heim.detachUI = function() {
-  uidocument.body.classList.remove('ready')
-  evs.removeAllEventListeners()
-  Heim.ui.unmountComponent()
-}
-
-Heim.prepareUpdate = function(hash) {
-  Heim.update.setReady(false)
-
-  var frame = uidocument.getElementById('env-update')
-  if (frame) {
-    frame.parentNode.removeChild(frame)
-  }
-
-  frame = uidocument.createElement('iframe')
-  frame.id = 'env-update'
-  frame.className = 'js'
-  uidocument.body.appendChild(frame)
-
-  frame.contentDocument.open()
-  var context = frame.contentWindow
-  context.onReady = function() {
-    var removeListener = context.Heim.chat.store.listen(function(chatState) {
-      if (chatState.joined) {
-        removeListener()
-
-        // let go of #container
-        Heim.detachUI()
-
-        // attach new React component to #container
-        context.Heim.attachUI(hash)
-        frame.id = 'env'
-
-        // goodbye, world!
-        window.frameElement.parentNode.removeChild(window.frameElement)
-      } else if (chatState.canJoin) {
-        Heim.update.setReady(true, context.Heim.actions.joinRoom)
-      } else {
-        Heim.update.setReady(false)
-      }
-    })
-    context.Heim.actions.connect(roomName)
-  }
-  context.document.write('<script src="/static/raven.js?v=' + hash +  '"></sc'+'ript>')
-  context.document.write('<script src="/static/main.js?v=' + hash +  '"></sc'+'ript>')
-  context.document.write('<link rel="stylesheet" type="text/css" id="css" href="/static/main.css?v=' + hash + '">')
-  context.document.close()
-}
-
-Heim.plugins.load(roomName)
-
-if (!window.onReady) {
-  Heim.actions.connect(roomName)
-  Heim.actions.joinRoom()
-}
-
-setImmediate(function() {
-  if (window.onReady) {
-    window.onReady()
-  } else {
-    Heim.attachUI()
-  }
-})
