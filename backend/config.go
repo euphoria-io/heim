affca41f816583f5aef11885c7326c6c539806bb
Logan Hanks <logan.hanks@gmail.com>
2015-03-16 16:48:00 -0700

    Go ahead and remove -etcd-peers.

--- backend/config.go
+++ backend/config.go
@@ -20,8 +20,6 @@ func init() {
 	flag.StringVar(&Config.Cluster.ServerID, "id", "singleton", "")
 	flag.StringVar(&Config.Cluster.EtcdHome, "etcd", "", "etcd path for cluster coordination")
 	flag.StringVar(&Config.Cluster.EtcdHost, "etcd-host", "", "address of a peer in etcd cluster")
-	// TODO: -etcd-peers is deprecated
-	flag.Var(&Config.Cluster.EtcdPeers, "etcd-peers", "comma-separated addresses of etcd peers")
 
 	flag.StringVar(&Config.DB.DSN, "psql", "", "")
 
@@ -91,21 +89,16 @@ func (cfg *ServerConfig) LoadFromFile(path string) error {
 }
 
 type ClusterConfig struct {
-	ServerID  string `yaml:"server-id"`
-	Era       string `yaml:"-"`
-	Version   string `yaml:"-"`
-	EtcdPeers CSV    `yaml:"-"`
-	EtcdHost  string `yaml:"etcd-host,omitempty"`
-	EtcdHome  string `yaml:"etcd,omitempty"`
+	ServerID string `yaml:"server-id"`
+	Era      string `yaml:"-"`
+	Version  string `yaml:"-"`
+	EtcdHost string `yaml:"etcd-host,omitempty"`
+	EtcdHome string `yaml:"etcd,omitempty"`
 }
 
 func (c *ClusterConfig) EtcdCluster() (cluster.Cluster, error) {
 	if c.EtcdHost == "" {
-		if len(c.EtcdPeers) > 0 {
-			c.EtcdHost = c.EtcdPeers[0]
-		} else {
-			return nil, fmt.Errorf("cluster: etcd-host must be specified")
-		}
+		return nil, fmt.Errorf("cluster: etcd-host must be specified")
 	}
 	return cluster.EtcdCluster(c.EtcdHome, c.EtcdHost, c.DescribeSelf())
 }
