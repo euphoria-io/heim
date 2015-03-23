51325b674b97e40a52a1a3e9f87044c2beeb8b22
Max Goodman <c@chromakode.com>
2015-03-23 10:08:03 -0700

    Log console messages in Sentry.

--- client/lib/raven.js
+++ client/lib/raven.js
@@ -1,5 +1,6 @@
 Raven = require('raven-js')
 require('raven-js/plugins/native')
+require('raven-js/plugins/console')
 Raven.config(process.env.SENTRY_ENDPOINT, {
   release: process.env.HEIM_RELEASE,
   tags: {'git_commit': process.env.HEIM_GIT_COMMIT},
