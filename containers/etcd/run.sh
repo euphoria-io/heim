020d660fa2d87011d40f7c14b0e85dc524a548be
Logan Hanks <logan.hanks@gmail.com>
2015-03-30 13:46:51 -0700

    Add --bind-addr to etcd run.

--- containers/etcd/run.sh
+++ containers/etcd/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 IP=$(nslookup $HOSTNAME | tail -n 1 | cut -d ' ' -f 3)
-ETCD_CMD="/bin/etcd -name etcd -data-dir /data -addr $IP:4001 $*"
+ETCD_CMD="/bin/etcd -name etcd -data-dir /data -addr $IP:4001 -bind-addr $IP:4001 $*"
 echo "Running: $ETCD_CMD"
 $ETCD_CMD
