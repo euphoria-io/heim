6797405f9ee98ed158efe8a4dfff2d9beb80bd79
Max Goodman <c@chromakode.com>
2015-04-03 18:13:17 -0700

    Remove origin restrictions for now.

--- client/lib/embed.js
+++ client/lib/embed.js
@@ -1,12 +1,6 @@
 var queryString = require('query-string')
 
 function render() {
-  if (location.origin != 'http://euphoria.local:8081' &&
-      top.location.origin != 'https://euphoria.io' &&
-      top.location.origin != 'http://euphoria.local:8080') {
-    return
-  }
-
   var data = queryString.parse(location.search)
 
   if (data.kind == 'youtube') {
