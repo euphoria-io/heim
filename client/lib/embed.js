a5d8559e354fdbce103c07fcab4059db723a9459
Max Goodman <c@chromakode.com>
2015-04-03 18:20:12 -0700

    Oops, fix origin of YouTube embeds.

--- client/lib/embed.js
+++ client/lib/embed.js
@@ -6,7 +6,7 @@ function render() {
   if (data.kind == 'youtube') {
     // jshint camelcase: false
     var embed = document.createElement('iframe')
-    embed.src = 'http://www.youtube.com/embed/' + data.youtube_id + (data.autoplay ? '?autoplay=1' : '')
+    embed.src = '//www.youtube.com/embed/' + data.youtube_id + (data.autoplay ? '?autoplay=1' : '')
     document.body.appendChild(embed)
   }
 }
