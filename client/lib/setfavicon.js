561a1484d0542c271a058843560e81223120e522
Max Goodman <c@chromakode.com>
2015-04-15 15:59:51 -0700

    Skip no-ops in setFavicon.

--- client/lib/setfavicon.js
+++ client/lib/setfavicon.js
@@ -4,8 +4,11 @@
 module.exports = function setFavicon(document, href) {
   var head = document.getElementsByTagName('head')[0]
   var faviconId = 'favicon'
-  var link = document.createElement('link')
   var oldLink = document.getElementById(faviconId)
+  if (oldLink.href == href) {
+    return
+  }
+  var link = document.createElement('link')
   link.id = faviconId
   link.rel = 'shortcut icon'
   link.href = href
