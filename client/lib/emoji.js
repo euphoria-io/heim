d25c3e5f76b48d90b7c25029e7db5ea7b01fed2d
Max Goodman <c@chromakode.com>
2015-05-05 17:59:36 -0700

    Add euphoric bronze emoji.

--- client/lib/emoji.js
+++ client/lib/emoji.js
@@ -5,7 +5,12 @@ module.exports = {}
 
 var index = module.exports.index = _.clone(require('emoji-annotation-to-unicode'))
 
+// (don't nag about object notation)
+// jshint -W069
 index['+1'] = 'plusone'
+index['bronze'] = 'bronze'
+index['bronze!?'] = 'bronze2'
+index['bronze?!'] = 'bronze2'
 
 module.exports.names = _.invert(index)
 
