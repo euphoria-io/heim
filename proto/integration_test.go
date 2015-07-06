0704523c4a6f1a75ac85a029d6dc017274ccd629
Logan Hanks <logan.hanks@gmail.com>
2015-07-06 15:50:08 -0700

    Add integration test for proto.

---
+++ proto/integration_test.go
@@ -0,0 +1,13 @@
+package proto_test
+
+import (
+	"testing"
+
+	"euphoria.io/heim/backend"
+	"euphoria.io/heim/backend/mock"
+	"euphoria.io/heim/proto"
+)
+
+func TestIntegration(t *testing.T) {
+	backend.IntegrationTest(t, func() proto.Backend { return &mock.TestBackend{} })
+}
