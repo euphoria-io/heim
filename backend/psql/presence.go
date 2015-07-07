cbbd082f4c13c2c64a80318a051e2b27d581b5e0
Logan Hanks <logan.hanks@gmail.com>
2015-07-07 16:07:19 -0700

    Remove unused roomPresence type.

--- backend/psql/presence.go
+++ backend/psql/presence.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"euphoria.io/heim/proto"
-	"euphoria.io/scope"
 )
 
 type Presence struct {
@@ -41,71 +40,4 @@ func (p *Presence) SessionView() (proto.SessionView, error) {
 type roomConn struct {
 	sessions map[string]proto.Session
 	nicks    map[string]string
-}
-
-type roomPresence map[string]roomConn
-
-func (rp roomPresence) load(sessionID string, userID proto.UserID, nick string) {
-	rc, ok := rp[string(userID)]
-	if !ok {
-		rc = roomConn{
-			sessions: map[string]proto.Session{},
-			nicks:    map[string]string{},
-		}
-		rp[string(userID)] = rc
-	}
-	rc.nicks[sessionID] = nick
-}
-
-func (rp roomPresence) join(session proto.Session) {
-	rp.load(session.ID(), session.Identity().ID(), session.Identity().Name())
-	rp[string(session.Identity().ID())].sessions[session.ID()] = session
-}
-
-func (rp roomPresence) part(session proto.Session) { delete(rp, session.ID()) }
-
-func (rp roomPresence) broadcast(ctx scope.Context, event *proto.Packet, exclude ...string) error {
-
-	payload, err := event.Payload()
-	if err != nil {
-		return err
-	}
-
-	exc := make(map[string]struct{}, len(exclude))
-	for _, x := range exclude {
-		exc[x] = struct{}{}
-	}
-
-	for _, rc := range rp {
-		for _, session := range rc.sessions {
-			if _, ok := exc[session.ID()]; ok {
-				continue
-			}
-
-			if err := session.Send(ctx, event.Type, payload); err != nil {
-				// TODO: accumulate errors
-				return fmt.Errorf("send message to %s: %s", session.ID(), err)
-			}
-		}
-	}
-
-	return nil
-}
-
-func (rp roomPresence) rename(nickEvent *proto.NickEvent) {
-	rc, ok := rp[string(nickEvent.ID)]
-	if !ok {
-		rc = roomConn{
-			sessions: map[string]proto.Session{},
-			nicks:    map[string]string{},
-		}
-		rp[string(nickEvent.ID)] = rc
-	}
-
-	for _, session := range rc.sessions {
-		if session.Identity().Name() == nickEvent.From {
-			session.SetName(nickEvent.To)
-			rc.nicks[session.ID()] = nickEvent.To
-		}
-	}
 }
