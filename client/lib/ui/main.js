7510c6be0c4b246fd0b290d2667c3ff6d23db938
Max Goodman <c@chromakode.com>
2015-07-14 13:52:37 -0700

    Hide sidebar UI and timestamps when viewport is thin.

--- client/lib/ui/main.js
+++ client/lib/ui/main.js
@@ -33,25 +33,25 @@ module.exports = React.createClass({
   ],
 
   componentWillMount: function() {
-    Heim.addEventListener(uiwindow, 'resize', this.onResize)
+    this._onResizeThrottled = _.throttle(this.onResize, 1000 / 30)
+    Heim.addEventListener(uiwindow, 'resize', this._onResizeThrottled)
     this.listenTo(this.state.ui.panes.get('popup').afterMessagesRendered, this.afterPopupMessagesRendered)
     this._threadScrollQueued = false
   },
 
   componentDidMount: function() {
     ui.focusEntry()
+    this.onResize()
   },
 
-  onResize: function(width) {
+  onResize: function() {
     // TODO
     //
     // this.props.onResize(node.offsetWidth, node.offsetHeight)
     //
     // update scrollers on resize too
-    return
     this.setState({
-      thin: width < 500,
-      wide: width > 920,
+      thin: uiwindow.innerWidth < 500,
     })
   },
 
@@ -198,7 +198,7 @@ module.exports = React.createClass({
     var threadPanes = this.state.ui.panes.filter((v, k) => /^thread-/.test(k))
     var extraPanes = this.templateHook('thread-panes')
     return (
-      <div id="ui" className={classNames({'disconnected': this.state.chat.connected === false, 'info-pane-hidden': !this.state.ui.infoPaneExpanded, 'info-pane-focused': this.state.ui.focusedPane == 'popup'})} onMouseDownCapture={this.onMouseDown} onClickCapture={this.onClick} onTouchMove={this.onTouchMove} onKeyDown={this.onKeyDown}>
+      <div id="ui" className={classNames({'disconnected': this.state.chat.connected === false, 'info-pane-hidden': this.state.thin || !this.state.ui.infoPaneExpanded, 'info-pane-focused': this.state.ui.focusedPane == 'popup'})} onMouseDownCapture={this.onMouseDown} onClickCapture={this.onClick} onTouchMove={this.onTouchMove} onKeyDown={this.onKeyDown}>
         {this.state.storage && this.state.storage.useOpenDyslexic && <link rel="stylesheet" type="text/css" id="css" href="/static/od.css" />}
           {this.state.chat.authState && this.state.chat.authState != 'trying-stored' && <div className="hatch-shade fill" />}
         <div className="info-pane" onMouseEnter={ui.freezeInfo} onMouseLeave={ui.thawInfo}>
@@ -210,8 +210,8 @@ module.exports = React.createClass({
           <NotificationList tree={this.state.chat.messages} pane={this.state.ui.panes.get('popup')} notifications={this.state.ui.frozenNotifications || this.state.notification.notifications} onNotificationSelect={this.onNotificationSelect} />
         </div>
         <div className="chat-pane-container main-pane">
-          <ChatTopBar who={this.state.chat.who} roomName={this.state.chat.roomName} connected={this.state.chat.connected} joined={this.state.chat.joined} authType={this.state.chat.authType} updateReady={this.state.update.get('ready')} working={this.state.chat.loadingLogs} sidebarExpanded={this.state.ui.infoPaneExpanded} collapseSidebar={ui.collapseInfoPane} expandSidebar={ui.expandInfoPane} />
-          <ChatPane pane={this.state.ui.panes.get('main')} showTimeStamps={true} onScrollbarSize={this.onScrollbarSize} />
+          <ChatTopBar who={this.state.chat.who} roomName={this.state.chat.roomName} connected={this.state.chat.connected} joined={this.state.chat.joined} authType={this.state.chat.authType} updateReady={this.state.update.get('ready')} working={this.state.chat.loadingLogs} showSidebarButton={!this.state.thin} sidebarExpanded={this.state.ui.infoPaneExpanded} collapseSidebar={ui.collapseInfoPane} expandSidebar={ui.expandInfoPane} />
+          <ChatPane pane={this.state.ui.panes.get('main')} showTimeStamps={!this.state.thin} onScrollbarSize={this.onScrollbarSize} />
           <div className="sidebar" style={{marginRight: this.state.scrollbarWidth}}>
             {this.templateHook('main-sidebar')}
           </div>
