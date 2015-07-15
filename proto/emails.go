fa993eb5a1e4cc488b6c0f3da73aeea49310fe75
Logan Hanks <logan.hanks@gmail.com>
2015-07-15 14:38:42 -0700

    Add testemail support for the actual templates.

--- proto/emails.go
+++ proto/emails.go
@@ -3,21 +3,79 @@ package proto
 import "euphoria.io/heim/proto/emails"
 
 const (
-	WelcomeEmail = emails.Template("welcome")
+	PasswordChanged       = emails.Template("password-changed")
+	PasswordReset         = emails.Template("password-reset")
+	RoomInvitation        = emails.Template("room-invitation")
+	RoomInvitationWelcome = emails.Template("room-invitation-welcome")
+	WelcomeEmail          = emails.Template("welcome")
 )
 
-var EmailScenarios = map[emails.Template]map[string]emails.TemplateTest{
-	WelcomeEmail: map[string]emails.TemplateTest{
-		"default": emails.TemplateTest{
-			Data: map[string]interface{}{
-				"EmailFromAddress":    "noreply@euphoria.io",
-				"EmailReplyToAddress": "help@euphoria.io",
-				"SiteName":            "Heim",
-				"SiteURL":             "https://heim.invalid",
-				"VerifyEmailURL":      "https://test.invalid/verify",
-				"ContactEmailAddress": "help@test.invalid",
-				"AccountEmailAddress": "user@somewhere.invalid",
+var (
+	EmailCommonData = commonData{
+		"EmailFromAddress": "noreply@euphoria.io",
+		"ReplyToAddress":   "help@euphoria.io",
+		"SiteName":         "heim",
+		"SiteURL":          "https://heim.invalid",
+	}
+
+	EmailScenarios = map[emails.Template]map[string]emails.TemplateTest{
+		WelcomeEmail: map[string]emails.TemplateTest{
+			"default": emails.TemplateTest{
+				Data: EmailCommonData.And(map[string]interface{}{
+					"VerifyEmailURL":      "https://test.invalid/verify",
+					"ContactEmailAddress": "help@test.invalid",
+					"AccountEmailAddress": "user@somewhere.invalid",
+				}),
 			},
 		},
-	},
+
+		PasswordChanged: map[string]emails.TemplateTest{
+			"default": emails.TemplateTest{
+				Data: EmailCommonData.And(map[string]interface{}{"AccountName": "yourname"}),
+			},
+		},
+
+		PasswordReset: map[string]emails.TemplateTest{
+			"default": emails.TemplateTest{
+				Data: EmailCommonData.And(map[string]interface{}{
+					"AccountName":      "yourname",
+					"PasswordResetURL": "https://test.invalid/reset",
+				}),
+			},
+		},
+
+		RoomInvitation: map[string]emails.TemplateTest{
+			"default": emails.TemplateTest{
+				Data: EmailCommonData.And(map[string]interface{}{
+					"SenderName":    "thatguy",
+					"RoomName":      "butts",
+					"RoomURL":       "https://heim.invalid/room/butts",
+					"SenderMessage": "hey, i heard you like butts",
+				}),
+			},
+		},
+
+		RoomInvitationWelcome: map[string]emails.TemplateTest{
+			"default": emails.TemplateTest{
+				Data: EmailCommonData.And(map[string]interface{}{
+					"SenderName":    "thatguy",
+					"RoomName":      "cabal",
+					"RoomPrivacy":   "private",
+					"RoomURL":       "https://heim.invalid/room/cabal",
+					"SenderMessage": "let's move our machinations here",
+				}),
+			},
+		},
+	}
+)
+
+type commonData map[string]interface{}
+
+func (cd commonData) And(data map[string]interface{}) map[string]interface{} {
+	for key, val := range cd {
+		if _, ok := data[key]; !ok {
+			data[key] = val
+		}
+	}
+	return data
 }
