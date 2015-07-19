d6fee4539c957b52d8b3740de6ad53ba37961fbe
Max Goodman <c@chromakode.com>
2015-07-19 12:32:48 -0700

    Rename :iphone: -> :mobile:.

--- client/lib/emoji.js
+++ client/lib/emoji.js
@@ -21,6 +21,9 @@ index['spider'] = 'spider'
 index['indigo_heart'] = 'indigo_heart'
 index['orange_heart'] = 'orange_heart'
 
+index['mobile'] = index['iphone']
+delete index['iphone']
+
 module.exports.names = _.invert(index)
 
 module.exports.codes = _.uniq(_.values(index))
