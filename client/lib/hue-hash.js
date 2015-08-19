c0e9bec1017d8ed2e52192fed96eda05054d06e1
Max Goodman <c@chromakode.com>
2015-08-19 15:16:31 -0700

    Add comments to explain hue hash algorithm.

--- client/lib/hue-hash.js
+++ client/lib/hue-hash.js
@@ -20,14 +20,27 @@ function hueHash(text, offset) {
     // scramble char codes across [0-255]
     // prime multiple chosen so @greenie can green, and @redtaboo red.
     var charVal = (text.charCodeAt(i) * 439) % 256
+
+    // multiply val by 33 while constraining within signed 32 bit int range.
+    // this keeps the value within Number.MAX_SAFE_INTEGER without throwing out
+    // information.
     var origVal = val
     val = val << 5
     val += origVal
+
+    // add the character information to the hash.
     val += charVal
   }
+
+  // cast the result of the final character addition to a 32 bit int.
   val = val << 0
+
+  // add the minimum possible value, to ensure that val is positive (without
+  // throwing out information).
   val += Math.pow(2, 31)
 
+  // add the calibration offset and scale within 0-254 (an arbitrary range kept
+  // for consistency with prior behavior).
   return (val + offset) % 255
 }
 
