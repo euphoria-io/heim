1867cce45cc474ff2a284931eb631a0a82d3e9ba
Max Goodman <c@chromakode.com>
2015-09-09 14:42:36 -0700

    Remove unused collapsed user list rendering code.

--- client/lib/ui/user-list.js
+++ client/lib/ui/user-list.js
@@ -10,20 +10,10 @@ module.exports = React.createClass({
 
   render: function() {
     var list
-    var remaining = 0
 
     list = this.props.users
       .toSeq()
       .filter(user => user.get('name'))
-
-    if (this.props.collapsed) {
-      var latest = list
-        .sortBy(user => -(user.get('lastSent') || 0))
-
-      latest.cacheResult()
-      remaining = latest.size - 4
-      list = latest.take(4)
-    }
 
     list = list.sortBy(user => user.get('name').toLowerCase())
 
@@ -32,7 +22,6 @@ module.exports = React.createClass({
         {list.map(function(user) {
           return <MessageText key={user.get('session_id')} className="nick" onlyEmoji={true} style={{background: 'hsl(' + user.get('hue') + ', 65%, 85%)'}} content={user.get('name')} />
         }, this).toArray()}
-        {remaining > 0 && <div className="more nick">+{remaining} more</div>}
       </div>
     )
   },
