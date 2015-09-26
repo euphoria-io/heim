9d9241a186d437a0d08aa4e4b55c1579ae9cb14e
Tim McCormack <cortex@brainonfire.net>
2015-09-25 23:31:23 -0400

    Sub-rank infix mention matches by position; final tie-break on name.
    Numeric scores are negated to match the lower-score-is-better behavior
    needed for comparing name strings directly (as fallback); to match this
    the sort is now done forwards instead of negating in the sortBy.
    
    We also switch to complex scores, lexicographically sorted, in order to
    support more nuanced infix comparisons.

--- client/lib/mention.js
+++ client/lib/mention.js
@@ -20,9 +20,10 @@ module.exports.containsSubseq = function(name, part) {
 }
 
 /**
- * From a name and a partial produce a score. A score of
- * zero indicates "no match whatsoever", all other scores
- * are positive.
+ * From a name and a partial produce a score. Scores are arrays of
+ * constant length and are intended to be compared lexicographically.
+ * Lower values are better matches. If the result is null, there is
+ * no match whatsoever.
  */
 module.exports.scoreMatch = function(name, part) {
   // FIXME Use proper Unicode-aware case-folding, if not already
@@ -35,25 +36,62 @@ module.exports.scoreMatch = function(name, part) {
   var indexOfCs = name.indexOf(part);
   var indexOfCi = nameLowercase.indexOf(partLowercase);
   if (indexOfCs === 0) {
-    return 31
+    return [-31, 0]
   } else if (indexOfCi === 0) {
-    return 30
+    return [-30, 0]
   } else if (indexOfCs >= 0) {
-    return 21
+    return [-21, indexOfCs]
   } else if (indexOfCi >= 0) {
-    return 20
+    return [-20, indexOfCi]
   } else if (module.exports.containsSubseq(name, part)) {
-    return 11
+    return [-11, 0]
   } else if (module.exports.containsSubseq(nameLowercase, partLowercase)) {
-    return 10
+    return [-10, 0]
   } else {
-    return 0
+    return null
   }
+}
+
+/**
+ * Yield {completion, score} for a pair of name, stripped
+ * partial name.
+ */
+function annotateScore(name, partStrip) {
+  var stripped = hueHash.stripSpaces(name)
+  var score = module.exports.scoreMatch(stripped, partStrip)
+  if (score) {
+    // Add tie-breakers. We first sort by lowercased names and
+    // then by the original names so that we don't get orderings
+    // like ["A", "Z", "a"]. This still sorts uppercase before
+    // lowercase, which is fine.
+    score.push(name.toLowerCase(), name)
+  }
+  return {completion: stripped, score: score}
+}
+
+/**
+ * Custom lexicographic comparator on pairs of equal-length
+ * arrays. Does not do a deep comparison on sub-arrays.
+ */
+function compareArrays(a, b) {
+  var len = a.length;
+  for (var i = 0; i < len; i++) {
+    var elA = a[i]
+    var elB = b[i]
+    if (elA < elB) {
+      return -1
+    } else if (elA > elB) {
+      return 1
+    }
+    // continue if equal...
+  }
+  return 0;
 }
 
 /**
  * Given an Immutable Seq of names and a partial name, yield sorted
  * Seq of mentionable names by match relevancy (best first).
+ * Names that do not match at all are omitted from the result.
  * Mentionable names are suitable for use as mentions (do not contain
  * spaces, but do contain emoji, non-ASCII, etc.)
  */
@@ -61,11 +99,14 @@ module.exports.rankCompletions = function(names, part) {
   var partStrip = hueHash.stripSpaces(part)
   return names
     .filter(Boolean)
-    .map(function(name) {
-      var stripped = hueHash.stripSpaces(name)
-      return [stripped, module.exports.scoreMatch(stripped, partStrip)]
-    })
-    .filter(entry => entry[1] > 0)
-    .sortBy(entry => -entry[1])
-    .map(entry => entry[0])
+    .map(name => annotateScore(name, partStrip))
+    .filter(entry => entry.score)
+    // Use a custom lexicographic array sorter because JS's native
+    // array comparison stringifies numeric elements for comparison,
+    // meaning negative numbers compare incorrectly. We need negative
+    // numbers in the score so that better matches come up at the
+    // front -- because that needs to match the tie-breaker of
+    // asciibetical ordering!
+    .sortBy(entry => entry.score, compareArrays)
+    .map(entry => entry.completion)
 }
