d977c32ede342cd2a5b26dafc47d454c33c36895
Tim McCormack <cortex@brainonfire.net>
2015-09-29 09:18:55 -0400

    Sub-rank subseq matches as well.
    - Phase-separated scoring around case
    - Moves compareArrays further up in order to compare case fold/keep scores
    - Replace containsSubseq with findSubseq

--- client/test/mention-test.js
+++ client/test/mention-test.js
@@ -5,57 +5,90 @@ var Immutable = require('immutable')
 describe('mention', function() {
   var mention = require('../lib/mention')
 
-  describe('containsSubseq', function() {
-    function assertContains(n, p, yes) {
-      assert.equal(mention.containsSubseq(n, p), yes)
+  describe('findSubseq', function() {
+    function assertFinding(n, p, start) {
+      assert.deepEqual(mention.findSubseq(n, p), start)
     }
 
     it('handles empty and exact matches', function() {
-      assertContains('', '', true)
-      assertContains('name', '', true)
-      assertContains('', 'cruft', false)
-      assertContains('name', 'name', true)
+      assertFinding('', '', -1)
+      assertFinding('name', '', -1)
+      assertFinding('', 'cruft', -1)
+      assertFinding('name', 'name', 0)
     })
     it('rejects extra on end of partial', function() {
-      assertContains('name', 'namex', false)
+      assertFinding('name', 'namex', -1)
     })
-    it('finds standard cases', function() {
-      assertContains('name', 'nm', true)
-      assertContains('name', 'e', true)
+    it('does not case-fold', function() {
+      assertFinding('name', 'NAME', -1)
+      assertFinding('NAME', 'name', -1)
+      assertFinding('NAME', 'NAME', 0)
+    })
+    it('yields correct start in corner cases', function() {
+      assertFinding('a', 'a', 0)
+      assertFinding('name', 'n', 0)
+      assertFinding('name', 'nm', 0)
+      assertFinding('name', 'e', 3)
+    })
+    it('finds earliest subseq', function() {
+      assertFinding('aaaa', 'a', 0)
+      assertFinding('namename', 'am', 1)
+      assertFinding('ABxxBxCxxD', 'BCD', 1)
     })
     // This is a possible index bug.
     it('rejects extra chars going off the end', function() {
-      assertContains('name', 'es', false)
+      assertFinding('name', 'es', -1)
     })
     it('respects order', function() {
-      assertContains('name', 'eman', false)
+      assertFinding('name', 'eman', -1)
     })
     it('respects count (always advance)', function() {
-      assertContains('name', 'nnm', false)
+      assertFinding('name', 'nnm', -1)
+    })
+  })
+
+  // These tests are more perfunctory since scoreMatch is the real
+  // deal; this is just a supporting fn.
+  describe('matchPinningCase', function() {
+    function check(name, prefix, match) {
+      assert.deepEqual(mention.matchPinningCase(name, prefix), match)
+    }
+    it('matches without case-folding', function() {
+      check('TimMc', 'timmc', null)
+      check('timmc', 'TimMc', null)
+      check('TimMc', 'TimMc', [-1, -1, 0])
+    })
+    it('prefers contiguous match over earlier subsequence', function() {
+      check('timmc', 'mc', [-1, 0, 3])
+      check('timxc', 'mc', [0, 0, 2])
     })
   })
 
   describe('scoreMatch', function() {
-    
+    function scoreEqual(name, prefix, score) {
+      assert.deepEqual(mention.scoreMatch(name, prefix), score)
+    }
+
     it('scores entirely bad matches as null', function() {
-      assert.deepEqual(mention.scoreMatch('', 'something'), null)
-      assert.deepEqual(mention.scoreMatch('1', '2'), null)
+      scoreEqual('', 'something', null)
+      scoreEqual('1', '2', null)
     })
-    it('empty prefix always matches', function() {
-      assert.deepEqual(mention.scoreMatch('', ''), [-31, 0])
-      assert.deepEqual(mention.scoreMatch('something', ''), [-31, 0])
+    it('empty prefix doesn\'t match', function() {
+      scoreEqual('', '', null)
+      scoreEqual('something', '', null)
     })
-    it('has expected score levels for a single name', function() {
+    it('produces lower scores for better matches', function() {
       // Exact match scores same as prefix for now.
-      assert.deepEqual(mention.scoreMatch('TimMc', 'TimMc'), [-31, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'timmc'), [-30, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'Tim'), [-31, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'tim'), [-30, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'imM'), [-21, 1])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'imm'), [-20, 1])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'TM'), [-11, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'tm'), [-10, 0])
-      assert.deepEqual(mention.scoreMatch('TimMc', 'no'), null)
+      scoreEqual('TimMc', 'TimMc', [-1, -1, -1, 0])
+      scoreEqual('TimMc', 'timmc', [-1, -1, 0, 0])
+      scoreEqual('TimMc', 'Tim', [-1, -1, -1, 0])
+      scoreEqual('TimMc', 'tim', [-1, -1, 0, 0])
+      scoreEqual('TimMc', 'imM', [-1, 0, -1, 1])
+      scoreEqual('TimMc', 'imm', [-1, 0, 0, 1])
+      scoreEqual('TimMc', 'mc', [-1, 0, 0, 3])
+      scoreEqual('TimMc', 'TM', [0, 0, -1, 0])
+      scoreEqual('TimMc', 'tm', [0, 0, 0, 0])
+      scoreEqual('TimMc', 'no', null)
     })
   })
 
@@ -77,7 +110,7 @@ describe('mention', function() {
       // we could treat prefix as a high-scoring infix.
       assertRanking(users, 'M', ['Max2', 'mac', 'TimMc', 'chromakode'])
     })
-    it('ranks subseqs less than infix ci', function() {
+    it('infix over subseq, even when earlier and case-match', function() {
       assertRanking(users, 'mc', ['TimMc', 'mac'])
     })
     it('sorts asciibetically as tie-breaker, caps first', function() {
@@ -88,8 +121,12 @@ describe('mention', function() {
       assertRanking(['aXX', 'bbXX'], 'XX', ['aXX', 'bbXX'])
       assertRanking(['aaXX', 'bXX'], 'XX', ['bXX', 'aaXX'])
     })
+    it('ranks earlier subseq over later', function() {
+      assertRanking(['xAxB', 'xxAxB'], 'ab', ['xAxB', 'xxAxB'])
+      assertRanking(['xBxA', 'xxBxA'], 'ba', ['xBxA', 'xxBxA'])
+    })
     it('strips spaces from names', function() {
-      assertRanking(users, 'x2', ['Max2'])
+      assertRanking([' Max 2 '], 'x2', ['Max2'])
     })
   })
 })
