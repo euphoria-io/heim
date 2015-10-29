1f23758171dca4ef91c37bc4dd0d884da4fde710
Max Goodman <c@chromakode.com>
2015-10-29 16:12:37 -0700

    Remove unnecessary injected http-equiv attribute.

--- client/emails/email/Email.js
+++ client/emails/email/Email.js
@@ -19,7 +19,7 @@ export default React.createClass({
     return (
       <html xmlns="http://www.w3.org/1999/xhtml">
         <head>
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <title>{this.props.title}</title>
         </head>
