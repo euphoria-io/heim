fd02fdb25b9ea0c7071c68b5a105ac7a7277e469
Max Goodman <c@chromakode.com>
2015-11-26 13:31:19 -0800

    Season's greetings

--- client/lib/fauxPlugins.js
+++ client/lib/fauxPlugins.js
@@ -652,4 +652,24 @@ export default function initPlugins(roomName) {
       )
     }
   }
+
+  Heim.hook('page-bottom', () => {
+    return (
+      <style key="winter-style" dangerouslySetInnerHTML={{__html: `
+        .messages-content {
+          background: white url(/static/winter.svg) no-repeat !important;
+          background-position: right 180px bottom 0 !important;
+          background-size: 700px !important;
+          background-attachment: fixed !important
+        }
+
+
+        @media (max-width: 650px) {
+          .messages-content {
+            background-size: 180px !important;
+          }
+        }
+      `}} />
+    )
+  })
 }
