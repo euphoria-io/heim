e2c76ce3804162864547cfe86b5bbda16529e00e
Max Goodman <c@chromakode.com>
2015-12-03 11:47:49 -0800

    Remove another link to email preferences page until it is implemented.

--- client/emails/common-txt.js
+++ client/emails/common-txt.js
@@ -1,9 +1,5 @@
 const standardFooter = `
 this message was sent to {{.AccountEmailAddress}} because an account is registered on {{.SiteURL}} with this email address.
-
-to change your email notification preferences, click here:
-
-{{.EmailPreferencesURL}}
 `.trim()
 
 export default { standardFooter }
