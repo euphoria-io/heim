8e91281c6780b6796f09699e2558be7a3ea95175
Max Goodman <c@chromakode.com>
2015-12-07 17:28:50 -0800

    Add UI to re-send verify email if not verified.

--- client/lib/stores/accountSettingsFlow.js
+++ client/lib/stores/accountSettingsFlow.js
@@ -15,6 +15,7 @@ const storeActions = Reflux.createActions([
   'changeName',
   'changeEmail',
   'changePassword',
+  'resendVerifyEmail',
   'resetPassword',
   'logout',
 ])
@@ -43,6 +44,8 @@ module.exports.store = Reflux.createStore({
     {changeEmailFailed: chat.changeEmail.failed},
     {changePasswordCompleted: chat.changePassword.completed},
     {changePasswordFailed: chat.changePassword.failed},
+    {resendVerifyEmailCompleted: chat.resendVerifyEmail.completed},
+    {resendVerifyEmailFailed: chat.resendVerifyEmail.failed},
     {resetPasswordCompleted: chat.resetPassword.completed},
     {resetPasswordFailed: chat.resetPassword.failed},
   ],
@@ -112,6 +115,15 @@ module.exports.store = Reflux.createStore({
     }))
   },
 
+  resendVerifyEmailCompleted() {
+    this.triggerUpdate(new StateRecord({step: 'verify-email-sent'}))
+  },
+
+  resendVerifyEmailFailed(data) {
+    this.triggerUpdate(this.state.set('working', false))
+    throw new Error('unable to resend verify email: ' + data.error)
+  },
+
   resetPasswordCompleted() {
     this.triggerUpdate(this.state.merge({
       step: 'reset-email-sent',
@@ -166,6 +178,14 @@ module.exports.store = Reflux.createStore({
       errors: Immutable.Map(),
     }))
     chat.changePassword(oldPassword, newPassword)
+  },
+
+  resendVerifyEmail() {
+    this.triggerUpdate(this.state.merge({
+      working: true,
+      errors: Immutable.Map(),
+    }))
+    chat.resendVerifyEmail()
   },
 
   resetPassword() {
