45f2a8811dc4a30873a6fac57cef2823ed060772
Max Goodman <c@chromakode.com>
2015-12-09 15:38:01 -0800

    Fix page location reading on update in Firefox.

--- client/lib/clientRoom.js
+++ client/lib/clientRoom.js
@@ -34,7 +34,7 @@ export default function clientRoom() {
     }
 
     // read url hash flags pertaining to socket connection
-    const roomName = location.pathname.match(/(\w+)\/$/)[1]
+    const roomName = uiwindow.location.pathname.match(/((pm:)?\w+)\/$/)[1]
     const hashFlags = queryString.parse(location.hash.substr(1))
     let connectEndpoint = process.env.HEIM_ORIGIN + process.env.HEIM_PREFIX
     if (process.env.NODE_ENV !== 'production' && hashFlags.connect) {
