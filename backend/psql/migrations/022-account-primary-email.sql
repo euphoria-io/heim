0c99379321e0bb1e128c5ea039bd5322a49d2fbe
Logan Hanks <logan.hanks@gmail.com>
2015-12-11 05:20:58 -0800

    Backfill primary email for unverified accounts.

--- backend/psql/migrations/022-account-primary-email.sql
+++ backend/psql/migrations/022-account-primary-email.sql
@@ -3,7 +3,8 @@ ALTER TABLE account ADD email TEXT NOT NULL DEFAULT '';
 
 UPDATE account SET email = (
     SELECT personal_identity.id FROM personal_identity
-        WHERE account_id = account.id AND namespace = 'email' and verified IS TRUE);
+        WHERE account_id = account.id AND namespace = 'email'
+        ORDER BY verified DESC LIMIT 1);
 
 -- +migrate Down
 ALTER TABLE account DROP IF EXISTS email;
