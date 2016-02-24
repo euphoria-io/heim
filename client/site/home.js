a7d537a405fae144983cbba6b6af20ad174debab
Logan Hanks <logan.hanks@gmail.com>
2016-02-23 18:31:53 -0800

    Remove chat style from landing page

--- client/site/home.js
+++ client/site/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { MainPage, FancyLogo, FauxMessage } from './common'
+import { MainPage, FancyLogo } from './common'
 import heimURL from '../lib/heimURL'
 
 
@@ -10,14 +10,12 @@ module.exports = (
       <FancyLogo />
       <div className="info-box">
         <div className="description">
-          <div className="messages">
-            <FauxMessage sender="euphoria" message="we're building a platform for cozy real time discussion spaces" />
-            <FauxMessage sender="euphoria" message="it's like a mix of chat, forums, and mailing lists">
-              <div className="replies">
-                <FauxMessage sender="euphoria" message="with your friends, organizations, and people around the world." />
-              </div>
-            </FauxMessage>
-          </div>
+          <p>
+            We're building a platform for cozy realtime discussion spaces.
+          </p>
+          <p>
+            It's like a mix of chat, forums, and mailing lists, with your friends, organizations, and people around the world.
+          </p>
           <a className="start-chatting big-green-button" href={heimURL('/room/welcome/')} target="_blank">come say hello!</a>
         </div>
       </div>
