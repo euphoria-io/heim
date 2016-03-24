17d51fcc814d25a8e2b17b4f74d33261bee62f5a
Logan Hanks <logan.hanks@gmail.com>
2016-03-24 11:38:46 -0700

    Add &rmusic and &listentothis as youtube rooms

--- client/lib/fauxPlugins.js
+++ client/lib/fauxPlugins.js
@@ -86,7 +86,7 @@ export default function initPlugins(roomName) {
     })
   }
 
-  if (roomName === 'music' || roomName === 'youtube') {
+  if (roomName === 'music' || roomName === 'youtube' || roomName === 'rmusic' || roomName === 'listentothis') {
     const Embed = require('./ui/Embed').default
     const MessageText = require('./ui/MessageText').default
 
