90eac31e92432649533055864087005011faa017
CylonicRaider <CylonicRaider@users.noreply.github.com>
2016-12-01 18:19:22 +0100

    Fix Go linker warning (#95)
    It's been there since... virtually forever, is mildly annoying, and
    might break things at some point.

--- backend/run.sh
+++ backend/run.sh
@@ -3,7 +3,7 @@
 set -ex
 
 go install \
-    -ldflags "-X main.version `git --git-dir=src/euphoria.io/heim/.git rev-parse HEAD`" \
+    -ldflags "-X main.version=`git --git-dir=src/euphoria.io/heim/.git rev-parse HEAD`" \
     euphoria.io/heim/heimctl
 
 export PATH=/go/bin:"$PATH"
