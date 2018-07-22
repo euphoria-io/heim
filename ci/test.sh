6d20c8dd7400f5f3dd693912d8dd549c3f6abbf0
Logan Hanks <logan.hanks@gmail.com>
2018-07-22 16:35:05 -0700

    Convert CI script to bazel

--- ci/test.sh
+++ ci/test.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 set -ex
 
-# pwd is gopath/src/euphoria-io/heim
-export HEIM_GOPATH=$(pwd)/../../..
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+curl --silent https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/bazel.list" \
+    -o Dir::Etc::sourceparts="-" \
+    -o APT::Get::List-Cleanup="0"
+sudo apt-get install openjdk-8-jdk bazel
 
-setup_deps() {
-  (cd client; npm install)
-  export PATH=$(pwd)/client/node_modules/.bin:${HEIM_GOPATH}/bin:${PATH}
-  export GOPATH=${HEIM_GOPATH}
-  ls -alF $(pwd)/client/node_modules/.bin
-}
-
-test_backend() {
-  psql -V
-  psql -c 'create database heimtest;' -U postgres -h $DB_HOST
-  export DSN="postgres://postgres@$DB_HOST/heimtest?sslmode=disable"
-  go get github.com/coreos/etcd
-  go install github.com/coreos/etcd
-  go test -v euphoria.io/heim/...
-}
-
-test_client() {
-  export NODE_ENV=development
-  pushd ./client
-  eslint ./
-  mochify
-  popd
-}
-
-setup_deps
-
-test_client
-test_backend
-
-if [ "$1" == "build" ]; then
-  $(dirname $0)/build.sh
-fi
+bazel test //...
